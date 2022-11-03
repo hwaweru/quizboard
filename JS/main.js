69854c1096d6fbeaee3d7bbb7df0c694552e990b
shiks <shiks@gmail.com>
2022-11-03 20:18:25 +0300

    amend code

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 // //const quizAnswers=document.querySelectorAll('.ouranswers');
 // //console.log(quizAnswers);
 // //const quizAnswers = document.querySelectorAll(".ouranswers");
-// let marks = ""
+// let marks = "0"
 // quizAnswers.forEach(function(jibu){
 //    jibu.addEventListener("change",function(event){
 //     alert("Selected");
