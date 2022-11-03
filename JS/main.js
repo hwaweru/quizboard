79491a2a5ba922b440d12e62cfc02e41ec67ef3e
shiks <shiks@gmail.com>
2022-11-03 20:00:11 +0300

    js edit

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+// //const quizAnswers=document.querySelectorAll('.ouranswers');
+// //console.log(quizAnswers);
+// //const quizAnswers = document.querySelectorAll(".ouranswers");
+// let marks = ""
+// quizAnswers.forEach(function(jibu){
+//    jibu.addEventListener("change",function(event){
+//     alert("Selected");
+//    });
+// });
