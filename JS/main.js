76c8bb5383ff67ece3bdff1b0e63948ce7493082
shiks <shiks@gmail.com>
2022-11-03 21:12:46 +0300

    add formula

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,17 @@
 //     alert("Selected");
 //    });
 // });
+const quizAnswers = document.querySelectorAll(".ouranswers");
+const submitAns=document.querySelector("#submitAns")
+let marks =0;
+let total=0;
+quizAnswers.forEach(function(jibu){
+    if(parseInt(jibu.value) > 0) {total += parseInt(jibu.value);}
+
+    else{marks = total;}
+   jibu.addEventListener("change",function(event){
+   marks += parseInt(event.target.value);
+   });;
+    submitAns.addEventListener("click",function(event){
+    alert("Your score is"+ marks)};
+
