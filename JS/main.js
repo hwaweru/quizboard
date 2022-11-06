374fbd16fd235ca7b910544e2522b50189a137d9
shiks <shiks@gmail.com>
2022-11-06 23:52:38 +0300

    file amends

--- JS/main.js
+++ JS/main.js
@@ -1,78 +1,40 @@
-// //const quizAnswers=document.querySelectorAll('.ouranswers');
-// //console.log(quizAnswers);
-// //const quizAnswers = document.querySelectorAll(".ouranswers");
-// let marks = "0"
-// quizAnswers.forEach(function(jibu){
-//    jibu.addEventListener("change",function(event){
-//     alert("Selected");
-//    });
-// });
-// const quizAnswers = document.querySelectorAll(".ouranswers");
-// const submitAns=document.querySelector("#submitAns")
-// let marks =0;
-// let total=0;
-// quizAnswers.forEach(function(jibu){
-//     if(parseInt(jibu.value) > 0) {total += parseInt(jibu.value);}
+$(document).ready(function () {
+  const quizAnswers = document.querySelectorAll(".ouranswers");
+  //const finishButton=document.querySelector("#AnsSheet")
+  const submitAnswer = document.querySelector("#submit_ans");
+  //const finalAnswer
 
-//     else{marks = total;}
-//    jibu.addEventListener("change",function(event){
-//    marks += parseInt(event.target.value);
-//    });;
-//     submitAns.addEventListener("click",function(event){
-//     alert("Your score is"+ marks)};
-$(document).ready(function() {
-const quizAnswers = document.querySelectorAll(".ouranswers");
-//const finishButton=document.querySelector("#AnsSheet")
-const submitAnswer = document.querySelector("#submit_ans");
-//const finalAnswer
+  let marks = 0;
 
-let marks = 0;
+  let total = 0;
 
-let total = 0;
-
-quizAnswers.forEach(function (jibu) {
-
-    if(parseInt(jibu.value) > 0) {
-
-        total += parseInt(jibu.value);
-
-    }
-
-    else{
-
-        total = total;
-
+  quizAnswers.forEach(function (jibu) {
+    if (parseInt(jibu.value) > 0) {
+      total += parseInt(jibu.value);
+    } else {
+      total = total;
     }
 
     jibu.addEventListener("change", function (event) {
-
-        marks += parseInt(event.target.value);
-
+      marks += parseInt(event.target.value);
     });
+  });
 
-});
-
-submitAnswer.addEventListener("click", function (event) {
-
-    let score = (marks/total)*100;
+  submitAnswer.addEventListener("click", function (event) {
+    let score = (marks / total) * 100;
     let level = "";
 
-
-    if (score > 80){
-        level += "Excellent";
+    if (score > 80) {
+      level += "Excellent";
+    } else if (score >= 50 && score <= 80) {
+      level += "Fair";
+    } else {
+      level += "Poor performance, Please retake test.";
     }
+    alert("Your score is "  + score + "%" + level);
 
-    else if(score >= 50 && score <= 80 ){
-        level += "Fair";
-    }
-
-    else{
-        level += "Poor performance, Please retake test.";
-
-    }
-    alert("Your score is " + score + "%" + level);
-    
     //$("#ansSheet").text("Your score is " + score + "%" + level");
     //$("#results").removeClass("d-none");
     //$("#finalScore").text("Your score is " + score + "%" + level)
-})});
+  });
+});
--- README.md
+++ README.md
@@ -18,7 +18,7 @@ My former high school has started an after-school computer club. The club is fin
 
 ## BDD
 
--See a small description of the Quiz Board on the landing page.
+-See an invitation to attempt the Javascript Language test on the Quiz Board on the landing page.
 -Read the question.
 -Select the correct answers.
 -Answer all questions.
